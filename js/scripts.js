55f33642fe6d49e300b9f784cf60ca188fb6ac8e
Code-Cacophony <57301331+Code-Cacophony@users.noreply.github.com>
2020-03-25 15:49:34 -0700

    add functions and modals to site

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+
+
+    
+
+    var personName = $("input#name").val();
+    var personDescription = $("input#description").val();
+    var personDate= $("input#date").val();
+    var personStart = $("input#start").val();
+    var personEnd = $("input#end").val();
+
+  
+
+    event.preventDefault(); 
+  });
+
+  $("#myBtn").click(function(){
+    $("#myModal").modal();
+    event.preventDefault(); 
+
+  });
+  
+
+});
